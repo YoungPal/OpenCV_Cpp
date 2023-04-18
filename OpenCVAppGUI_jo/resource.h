b082e07041302eb432fe30b5c4c40465697f0715
YoungPal <jygus513@gmail.com>
2023-04-18 09:09:05 +0900

    upload ver1.0

---
+++ OpenCVAppGUI_jo/resource.h
@@ -0,0 +1,34 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// OpenCVAppGUI.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_OPENCVAPPGUI_DIALOG         102
+#define IDP_SOCKETS_INIT_FAILED         103
+#define IDR_MAINFRAME                   128
+#define IDC_BTN_Load                    1013
+#define IDC_BTN_Save                    1014
+#define IDC_PICTURE                     1015
+#define IDC_PICTURE2                    1016
+#define IDC_PICTURE_RESULT              1016
+#define IDC_BTN_CH                      1017
+#define IDC_BTN_INSPECTION              1017
+#define IDC_PICTURE3                    1018
+#define IDC_PICTURE_DEBUG               1018
+#define IDC_INPUT                       1019
+#define IDC_OUTPUT                      1020
+#define IDC_OUTPUT_RESULT               1020
+#define IDC_INPUT_DEBUG                 1021
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1020
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
